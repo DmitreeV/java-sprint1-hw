8e43a7beaba8ce8b9ab70372b694ecdb215d8b9c
DmitreeV <color.ivanov2016@yandex.ru>
2022-05-11 22:21:19 +0300

    Внес заключтельные правки. Спасибо за уделенное время на проверку моего творения =) Хорошего вечера!

--- src/StepTracker.java
+++ src/StepTracker.java
@@ -13,30 +13,24 @@ public class StepTracker {
 
     public void saveSteps() {
         System.out.println("За какой месяц вы хотите ввести шаги?");
-        System.out.println("Введите номер месяца в формате : 1-Январь, 2-Февраль, 3-Март, 4-Апрель, 5-Май, 6-Июнь, 7-Июль, 8-Август, 9-Сентябрь, 10-Октябрь, 11-Ноябрь, 12-Декабрь. ");
+        monthSelection();
         int month = this.scanner.nextInt() - 1;
-        if (month < 0) {
-            System.out.println("Неверный формат. Введите значение от 1 до 12.");
-        } else if (month > 11) {
+        if (month < 0 || month > 11) {
             System.out.println("Неверный формат. Введите значение от 1 до 12.");
         } else {
             System.out.println("За какой день вы хотите ввести шаги?");
             System.out.println("Введите день в котором хотите сохранить количество шагов (от 1 до 30) :");
             int day = this.scanner.nextInt() - 1;
-            if (day < 0) {
+            if (day < 0 || day > 29) {
                 System.out.println("Значение не корректно, введите число от 1 до 30.");
             } else {
-                if (day > 29) {
-                    System.out.println("Значение не корректно, введите число от 1 до 30.");
+                System.out.println("Введите пройденное количество шагов за день:");
+                int steps = this.scanner.nextInt();
+                if (steps < 0) {
+                    System.out.println("Количество шагов не может быть отрицательным .");
                 } else {
-                    System.out.println("Введите пройденное количество шагов за день:");
-                    int steps = this.scanner.nextInt();
-                    if (steps < 0) {
-                        System.out.println("Количество шагов не может быть отрицательным .");
-                    } else {
-                        System.out.println("Выбран месяц " + (month + 1) + ". День " + (day + 1) + ". Количество шагов " + steps + ".");
-                        monthToData[month][day] = steps;
-                    }
+                    System.out.println("Выбран месяц " + (month + 1) + ". День " + (day + 1) + ". Количество шагов " + steps + ".");
+                    monthToData[month][day] = steps;
                 }
             }
         }
@@ -50,7 +44,7 @@ public class StepTracker {
 
 
         System.out.println("За какой месяц вы хотите посмотреть статистику?");
-        System.out.println("Введите номер месяца в формате : 1-Январь, 2-Февраль, 3-Март, 4-Апрель, 5-Май, 6-Июнь, 7-Июль, 8-Август, 9-Сентябрь, 10-Октябрь, 11-Ноябрь, 12-Декабрь. ");
+        monthSelection();
         int month = this.scanner.nextInt() - 1;
         if (month < 0) {
             System.out.println("Неверный формат. Введите значение от 1 до 12.");
@@ -61,7 +55,7 @@ public class StepTracker {
         } else {
             for (int i = 0; i < 30; i++) {
 
-                sum = sum + monthToData[month][i];
+                sum += monthToData[month][i];
 
                 if (monthToData[month][i] > maxSteps) {
                     maxSteps = monthToData[month][i];
@@ -89,16 +83,24 @@ public class StepTracker {
 
     }
 
-    public void newPurposeSteps() {
+    public int newPurposeSteps() {
 
-        System.out.println("Ваша дневная цель шагов 10000.");
+        System.out.println("Ваша дневная цель шагов : " + purposeSteps);
         System.out.println("Введите новую цель шагов : ");
-        int purposeSteps = scanner.nextInt();
-        if (purposeSteps < 0) {
+        int goalSteps = scanner.nextInt();
+
+        if (goalSteps < 0) {
             System.out.println("Цель шагов не может быть отрицательной, введите корректное значение!");
         } else {
+            purposeSteps = goalSteps;
             System.out.println("Ваша новая цель шагов : " + purposeSteps + ".");
+
         }
+        return purposeSteps;
+    }
+
+    public void monthSelection() {
+        System.out.println("Введите номер месяца в формате : 1-Январь, 2-Февраль, 3-Март, 4-Апрель, 5-Май, 6-Июнь, 7-Июль, 8-Август, 9-Сентябрь, 10-Октябрь, 11-Ноябрь, 12-Декабрь. ");
     }
 }
 
